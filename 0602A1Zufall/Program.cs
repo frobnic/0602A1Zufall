ad722bdd40cfd16b527a7154e98eaa2ab54131f9
frank paulsen <frobnic+ita@gmail.com>
2019-08-08 14:22:35 +0200

    code inserted

--- 0602A1Zufall/Program.cs
+++ 0602A1Zufall/Program.cs
@@ -10,6 +10,18 @@ namespace _0602A1Zufall
     {
         static void Main(string[] args)
         {
+            uint[] feld = new uint[100000];
+            Random zuf = new Random();
+            uint a;
+
+            for (int i = 0; i < 100000; i++)
+            {
+                feld[i] = (uint)zuf.Next(1, 50001);
+            }
+
+            Console.WriteLine("Bitte geben Sie eine Zahl zwischen 1 und 50000 ein");
+            a = uint.Parse(Console.ReadLine());
+
         }
     }
 }
